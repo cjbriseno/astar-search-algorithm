7b584b5c995020c18ec8fdff41c6b04b190e3d1b
cjbriseno <fr3shtricks@gmail.com>
2024-03-28 04:39:05 -0700

    new algo

---
+++ main.py
@@ -0,0 +1,17 @@
+from pathfinding import A_Star
+
+start1 = (0, 0)
+goal1 = (3, 2)
+grid = [
+    ['.', '.', '.', '.', '.'],
+    ['.', '.', '#', '#', '.'],
+    ['.', '.', '#', '.', '.'],
+    ['.', '#', '.', '.', '.'],
+    ['.', '#', '.', '.', '.']
+]
+print("Starting....")
+a = A_Star(start1, goal1, grid)
+path = a.astar_serach()
+if path:
+    for i in range(len(path)):
+        print("{0}){1}".format(i, path[i]))
